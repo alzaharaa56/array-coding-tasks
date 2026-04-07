c75585bbc31ff1649f705f384261551161fa0a75
alzahara <shahdaljabri33@gmail.com>
2026-04-07 09:01:55 +0400

    task 22

---
+++ src/task22.java
@@ -0,0 +1,9 @@
+public class task22 {
+    public static boolean double23(int[] nums) {
+if (nums.length == 2) {
+
+return (nums[0] == 2 && nums[1] == 2) || (nums[0] == 3 && nums[1] == 3);
+}
+return false;
+    }
+}
