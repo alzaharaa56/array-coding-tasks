36bd980336ddc2568833ac6bbc8ea4030b033f9e
alzahara <shahdaljabri33@gmail.com>
2026-04-07 09:04:47 +0400

    task 24

---
+++ src/task24.java
@@ -0,0 +1,16 @@
+public class task24 {
+    public static int[] swapEnds(int[] nums) {
+
+int temp = nums[0];
+
+
+
+nums[0] = nums[nums.length - 1];
+
+
+
+nums[nums.length - 1] = temp;
+
+return nums;
+    }
+}
