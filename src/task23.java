514db554e36acfe6b14023684bd6d98a155f175e
alzahara <shahdaljabri33@gmail.com>
2026-04-07 09:03:22 +0400

    task 23

---
+++ src/task23.java
@@ -0,0 +1,11 @@
+public class task23 {
+    public static int[] biggerTwo(int[] a, int[] b) {
+int sumA = a[0] + a[1];
+int sumB = b[0] + b[1];
+
+if (sumB > sumA) {
+return b;
+}
+return a;
+    }
+}
